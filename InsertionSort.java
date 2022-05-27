2c40c70baa54d8aaeb03dc0806f41bcfd8c9e169
birrusahithi <106225654+birrusahithi@users.noreply.github.com>
2022-05-27 11:44:32 -0700

    Insertion sort

---
+++ InsertionSort.java
@@ -0,0 +1,14 @@
+public class InsertionSort {
+    public void insertionSort(int [] arr){
+
+    }
+    public void printArray(int[] arr){
+
+    }
+    public static void main(String[] args){
+        InsertionSort obj = new InsertionSort();
+        int arr[] = {9, 10,1,2,13, 4,0,6};
+        obj.insertionSort(arr);
+        obj.printArray(arr);
+    }
+}
